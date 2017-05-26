141470072ac6bdad8df560ddb1540b46dfa54954
Antonis Kalou <kalouantonis@gmail.com>
2017-05-26 17:36:19 +0200

    Added docs for go-when

--- src/johan_sports/busboy/async.clj
+++ src/johan_sports/busboy/async.clj
@@ -1,6 +1,7 @@
 (ns johan-sports.busboy.async)
 
 (defmacro go-when
+  "Continue running go block as long as `bindings` return true."
   [bindings & body]
   `(cljs.core.async.macros/go-loop []
      (when-let ~bindings
